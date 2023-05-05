a64dfdeb39d28a264b848f41d5119e82815c34c1
RashMaunu <121867629+RashMaunu@users.noreply.github.com>
2023-05-05 13:26:01 +0530

    Add files via upload

---
+++ final.tf
@@ -0,0 +1,87 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg"{
+  name  = "my-resource-group"
+  location = "eastus"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "my-virtual-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                  = "my-subnet"
+  resource_group_name   = azurerm_resource_group.rg.name
+  virtual_network_name  = azurerm_virtual_network.vnet.name
+  address_prefixes      = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                      = "example-nic"
+  location                  = "eastus"
+  resource_group_name       = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "my-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "BlockOutboundInternet"
+    priority                   = 100
+    direction                  = "Outbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "Internet"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = "my-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "adminr"
+  admin_password      = "Rash@1234"
+
+  os_disk {
+    caching                = "ReadWrite"
+    storage_account_type   = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+}
